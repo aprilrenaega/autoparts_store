ded795049223b32572698aba23185c1f2f3ae5f0
aprilrenaega <dnasmith@Aprils-MacBook-Pro.local>
2014-03-31 11:28:36 -0400

    Initial commit, changes required

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AutopartsStore::Application.config.secret_key_base = '9537fdf8293573d260d025b27c16e784b38efe6d09803db6cdf9268b4c2a189e94945784888762eb6c45700822d2c0b32236115ac95739bc43e4292f07504370'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AutopartsStore::Application.config.session_store :cookie_store, key: '_autoparts_store_session'
